bdf697aaa98af730337a461148598e71e8124621
Christoph Reimers <christoph@superservice-international.com>
2018-01-05 00:27:33 +0100

    replace some broken letters

--- index.js
+++ index.js
@@ -10,7 +10,13 @@ const removeEncryption = () => {
   encrypted.each((index, elm) => {
     let content = $(elm).html();
     let decrypted = content.replace(/[a-zA-Z]/g,function(c){return String.fromCharCode((c<="Z"?90:122)>=(c=c.charCodeAt(0)+25)?c:c-26);});
-    $(elm).html(decrypted)
+    decrypted = decrypted.replace(/\{/g, 'z');
+    decrypted = decrypted.replace(/å/g, 'ä');
+    decrypted = decrypted.replace(/Å/g, 'Ä');
+    decrypted = decrypted.replace(/ý/g, 'ü');
+    decrypted = decrypted.replace(/Ý/g, 'ü');
+    decrypted = decrypted.replace(/à/g, 'ß');
+    $(elm).html(decrypted);
   })
 
 }
